5768d10bf326e10991fcd4a065dbc5aecf85154c
UnderProgramer <ajdjskq13324@gmail.com>
2026-05-19 14:08:00 +0900

    add vercel dashboard

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    headers: {
+      "Content-Security-Policy": "connect-src 'self' http://43.203.78.189:8081"
+    }
+  }
 });
